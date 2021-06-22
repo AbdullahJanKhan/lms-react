8ddc80463f9b48e852f516fe7303bc877f580543
AbdullahJanKhan <abdullah.jankhan445@hotmail.com>
2021-06-22 11:52:18 +0500

    Assignment 5 React LMS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Mark from './Components/markAssignment';
+import Upload from './Components/uploadAssignment';
+import Delete from './Components/deleteAssignment';
+import Submit from './Components/submitAssignment';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route to='/'> <Upload /> </Route>
+          <Route to='/submit'> <Submit /> </Route>
+          <Route to='/mark'> <Mark /> </Route>
+          <Route to='/delete'> <Delete /> </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
